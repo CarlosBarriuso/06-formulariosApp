4eb3f967fbb42876d1a969934c129aa23894a24f
cbarriuso <carlosbarriuso@yahoo.es>
2023-03-28 12:54:54 +0200

    Validacion mayor que 0

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -7,19 +7,24 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./basicos.component.css'],
 })
 export class BasicosComponent {
-
   //Para traer la referencia local del html
   @ViewChild('miFormulario') miFormulario!: NgForm;
 
   //Para hacer la validadcion del campo producto del html
-  nombreValido():boolean {
-    return this.miFormulario?.controls['producto']?.invalid &&
-    this.miFormulario?.controls['producto']?.touched
+  nombreValido(): boolean {
+    return (
+      this.miFormulario?.controls['producto']?.invalid &&
+      this.miFormulario?.controls['producto']?.touched
+    );
   }
 
+  precioValido(): boolean {
+    //console.log(this.miFormulario?.controls['precio']?.value);
+    return this.miFormulario?.controls['precio']?.touched && this.miFormulario?.controls['precio']?.value < 0 ;
+  }
 
   //guardar(miFormulario: NgForm) {
-    guardar() {
+  guardar() {
     console.log(this.miFormulario);
   }
 }
