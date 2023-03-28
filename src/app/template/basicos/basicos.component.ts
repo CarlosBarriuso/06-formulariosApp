4339d1f0b2306753248a8f88ed93392f4e610724
cbarriuso <carlosbarriuso@yahoo.es>
2023-03-28 10:26:12 +0200

    ViewChild - Acceder a referencia local del html

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
-  styleUrls: ['./basicos.component.css']
+  styleUrls: ['./basicos.component.css'],
 })
 export class BasicosComponent {
-guardar(miFormulario: NgForm) {
-console.log(miFormulario.value);
-}
 
+  //Para traer la referencia local del html
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
+  //Para hacer la validadcion del campo producto del html
+  nombreValido():boolean {
+    return this.miFormulario?.controls['producto']?.invalid &&
+    this.miFormulario?.controls['producto']?.touched
+  }
+
+
+  //guardar(miFormulario: NgForm) {
+    guardar() {
+    console.log(this.miFormulario);
+  }
 }
