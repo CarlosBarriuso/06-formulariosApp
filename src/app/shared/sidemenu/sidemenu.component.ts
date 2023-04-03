716073a3cd1f5ef028520e15b6c42a46c7024aa1
cbarriuso <carlosbarriuso@yahoo.es>
2023-04-03 11:59:44 +0200

    Radio check y switch

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -8,7 +8,12 @@ interface MenuItem {
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
-  styleUrls: ['./sidemenu.component.css']
+  styles: [
+    `li{
+      cursor:pointer
+    }`
+  ]
+
 })
 export class SidemenuComponent {
 
