b3d4c2adf06cbac44f10708f50ddf34317b5e895
cbarriuso <carlosbarriuso@yahoo.es>
2023-04-13 11:48:29 +0200

    Validación de un campo reactivo

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./dinamicos.component.css']
 })
 export class DinamicosComponent {
-a:number =0;
+
+  a:number = 0;
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [ '', [ Validators.required, Validators.minLength(3) ] ],
+  });
+
+  constructor( private fb: FormBuilder ) {}
+
+  campoEsValido( campo:string){
+    return this.miFormulario.controls[campo].errors
+          && this.miFormulario.controls[campo].touched;
+  }
+
+  guardar(){
+
+    if ( this.miFormulario.invalid ){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value);
+}
 }
